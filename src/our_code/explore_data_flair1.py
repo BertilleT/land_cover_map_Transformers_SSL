d2943376fb76b9f11a7b8b93c708172f56bcbad8
Bertille Temple <bertilletemple@yahoo.fr>
2023-12-18 11:21:20 +0100

    add plots for classes proportions in flair 1 train valid and test

--- src/our_code/explore_data_flair1.py
+++ src/our_code/explore_data_flair1.py
@@ -4,25 +4,10 @@ import matplotlib.pyplot as plt
 import rasterio
 from pathlib import Path
 from utils import *
+import numpy as np
 
-folder_path = '../data/flair1/'
-
-images = sorted(list(get_data_paths(Path(folder_path), 'IMG*.tif')), key=lambda x: int(x.split('_')[-1][:-4]))
-masks  = sorted(list(get_data_paths(Path(folder_path), 'MSK*.tif')), key=lambda x: int(x.split('_')[-1][:-4]))
-
-image_file = images[0]
-mask_file = masks[0]
-
-img = read_image_file(image_file)
-# Print the shape of the image array
-print('image shape:', img.shape) #13*256*256
-
-msk = read_image_file(mask_file)
-print('mask shape:', msk.shape) #256*256
-
-plot_image_mask(img, msk)
-
-current_classes = {
+folder_path = '../../data/flair1/'
+dict_classes = {
 1   : 'building',
 2   : 'pervious surface',
 3   : 'impervious surface',
@@ -42,3 +27,58 @@ current_classes = {
 17  : 'ligneous',
 18  : 'greenhouse',
 19  : 'other'}
+
+colors = {
+1   : '#db0e9a',
+2   : '#938e7b',
+3   : '#f80c00',
+4   : '#a97101',
+5   : '#1553ae',
+6   : '#194a26',
+7   : '#46e483',
+8   : '#f3a60d',
+9   : '#660082',
+10  : '#55ff00',
+11  : '#fff30d',
+12  : '#e4df7c',
+13  : '#3de6eb',
+14  : '#ffffff',
+15  : '#8ab3a0',
+16  : '#6b714f',
+17  : '#c5dc42',
+18  : '#9999ff',
+19  : '#000000'}
+
+images = sorted(list(get_data_paths(Path(folder_path), 'IMG*.tif')), key=lambda x: int(x.split('_')[-1][:-4]))
+masks  = sorted(list(get_data_paths(Path(folder_path), 'MSK*.tif')), key=lambda x: int(x.split('_')[-1][:-4]))
+
+#split into train, validation and test
+from sklearn.model_selection import train_test_split
+images_train, images_test = train_test_split(images, test_size=0.2, random_state=42)
+masks_train, masks_test = train_test_split(masks, test_size=0.2, random_state=42)
+images_train, images_val = train_test_split(images_train, test_size=0.2, random_state=42)
+masks_train, masks_val = train_test_split(masks_train, test_size=0.2, random_state=42)
+
+#train classes balance
+per_classes_dict = per_classes(masks_train, dict_classes)
+plot_per_classes(per_classes_dict, dict_classes, colors, title = 'training set')
+
+#validation classes balance
+per_classes_dict = per_classes(masks_val, dict_classes)
+plot_per_classes(per_classes_dict, dict_classes, colors, title = 'validation set')
+
+#test classes balance
+per_classes_dict = per_classes(masks_test, dict_classes)
+plot_per_classes(per_classes_dict, dict_classes, colors, title = 'test set')
+
+
+'''img = read_image_file(image_file)
+# Print the shape of the image array
+print('image shape:', img.shape) #13*256*256
+
+msk = read_image_file(mask_file)
+print('mask shape:', msk.shape) #256*256
+
+plot_image_mask(img, msk)'''
+
+
--- src/our_code/utils.py
+++ src/our_code/utils.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import rasterio
 from pathlib import Path
+import numpy as np
+
 
 def get_data_paths(path, filter):
     for path in Path(path).rglob(filter):
@@ -17,3 +19,34 @@ def read_image_file(file):
     with rasterio.open(file) as src:
         image = src.read()
         return image
+    
+def per_classes(masks, dict_classes): 
+    #create empty dict with class number as key and 0 as values
+    class_per = dict.fromkeys(range(1,20), 0)
+    total_pixels = 0
+    for mask_file in masks:
+        msk = read_image_file(mask_file)
+        msk = msk.flatten()
+        unique, counts = np.unique(msk, return_counts=True)
+        for i in range(len(unique)):
+            class_per[unique[i]] += counts[i]
+        total_pixels += len(msk)
+
+    #filter my_dict of value 0.00
+    class_per = {k: v for k, v in class_per.items() if v > 0.00}
+    #calculate percentage and round to 2 decimals
+    class_per = {k: np.round(v/total_pixels, 4) for k, v in class_per.items()}
+    return class_per
+
+def plot_per_classes(class_per, dict_classes, colors, title = 'dataset'):
+    #make a bar plot with name class in x and per in y using lut_colorq 
+    #and write the percentage value for each bar legend
+
+    bars = plt.bar(class_per.keys(), class_per.values(), color=colors.values())
+    plt.title('Class percentage in ' + title)
+    plt.xticks(range(1,20), dict_classes.values(), rotation=90)
+    plt.ylabel('percentage')
+    for bar in bars:
+        yval = bar.get_height()
+        plt.text(bar.get_x()+ .1, yval + .005, str(round(yval*100, 2)) + '%')
+    plt.show()
